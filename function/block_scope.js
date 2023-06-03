fc8bf12b07a5562432aa0776d80a8429d2a58a8d
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-06-03 08:59:24 +0530

    scope of the function is completed

---
+++ function/block_scope.js
@@ -0,0 +1,34 @@
+// here we see the block scope in the js 
+// block scope 
+// let and const are the block scope in the  js 
+
+// three type of scope in the js 
+// local scope  variables declare inside the function 
+
+// block scope 
+// global scope 
+
+// let keywords does't belong to the window object 
+
+var v = 33; 
+console.log(v);
+
+
+
+// {
+//     let x = 3;
+//     console.log(x);
+// }
+
+// console.log(x);// this will gives the error 
+
+
+{
+    const data = "ashutosh kumar";
+    console.log(data);
+}
+// console.log(data);
+
+
+
+
