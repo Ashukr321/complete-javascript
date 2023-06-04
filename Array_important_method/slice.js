f6601aa582ba72b3e41262565ca7b4779c07c466
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-06-04 14:52:43 +0530

    slice methods is completed

---
+++ Array_important_method/slice.js
@@ -0,0 +1,12 @@
+//  here we see the slice methods in the js 
+
+// slice methods print the element of the shallow portion of the array in to the new array object 
+// array is reference type of the data 
+//create the array 
+const num = [3.4,3,4,5,6,75,4,3];
+const data  =num.slice(2);
+
+
+// if we not include  end point then this will print the all elments of the array 
+
+console.log(data.length);
