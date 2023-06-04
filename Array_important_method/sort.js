20e65cfd0c94863c7bc62104f5b1d18ffa533b7f
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-06-04 14:23:27 +0530

    sort methods is completed

---
+++ Array_important_method/sort.js
@@ -0,0 +1,25 @@
+// here we see the sort methods in the js 
+// sort methods sort the all the  elements in the accending order 
+
+// return new  sorted array 
+
+//create the array 
+const arr= [3,2,5,5];
+
+arr.sort();
+console.log(arr);// 2  3 5 5
+arr.forEach((data)=>{
+    console.log(data); 
+})
+
+// example of the string in the js 
+const city = ["patna","delhi","gujrat","jammu kashmir"];
+// here we see the city is sort in order to alphabhetical 
+
+city.sort();
+console.log(city);
+
+console.log("after revesring the array elements ");
+city.reverse();
+
+console.log(city);
