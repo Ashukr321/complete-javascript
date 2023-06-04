fc3a13262e45e24f6436ce340df2816b4ec36ef8
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-06-04 23:44:25 +0530

    iterables is completed

---
+++ importants_topic/iterables.js
@@ -0,0 +1,37 @@
+// iterables means simple  which can be traverse easily 
+
+// example of iterables are 
+// array and string 
+
+
+
+//or which we can apply  the concepts of the looping 
+
+//example  of the array 
+const data = [3,2,4,5,6];
+
+console.log(data);
+
+data.forEach((elements)=>{
+    console.log(elements);
+})
+
+// here we create the string 
+const hi = "Ashutosh kunar";
+
+
+// hi.length
+// console.log("\n");
+// console.log(hi[0]);
+// console.log(hi[1]);
+// console.log(hi[2]);
+// console.log(hi[9]);
+
+
+
+
+for(let x of hi){
+    console.log(x);
+}
+
+
