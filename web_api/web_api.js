1b8d89295c5dc0c642c24c7c02f18afff85f5afe
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-06-07 07:25:01 +0530

    basic of web api is completed

---
+++ web_api/web_api.js
@@ -0,0 +1,40 @@
+// here we see the web api  in the js 
+
+/*
+
+it can extends the functionality of the browser 
+it can greatly simplify complex function 
+it can provide easy syntax to complex code 
+
+api => application programming interface 
+
+A browser can extends functinality of a web browser 
+a server api can extends the functionality of a web  server
+
+
+
+all browser have inbuilt some set of web api's to support complex operation  and to help accessing data 
+example geolocation api 
+
+
+3rd party api which is not built in your browser 
+  to use these all api we have to download the code from the web 
+
+  example youtube api 
+  twitter api 
+  facebook api 
+
+
+  // fetch api 
+
+
+*/
+// 1 fetch api 
+
+// this is very most important web api fetch that allow web browser to make http request to web server 
+
+// in this no need for xmlhtttpRequest anymore 
+
+// here we come after now compition of asyn and awiat
+
+
